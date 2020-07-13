962111f2645e478536524dab38c2fcb7a48a8a84
Marian Focsa <ext-marian.focsa@flowbird.group>
2020-07-13 16:57:09 +0300

    Add basic RabbitMQ 'hello haters' communication example with BasicPublish && BasicConsume

---
+++ RabbitMqIntro/RabbitMqIntro.Receive/Program.cs
@@ -0,0 +1,46 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace RabbitMqIntro.Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // declare the queue (idempotent -> same action always; if it exists, it uses it)
+                    // must be the same channel as declared in the publisher
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += Consumer_Received;
+                    channel.BasicConsume(queue: "hello",
+                                        autoAck: true,
+                                        consumer: consumer);
+
+                    // note: block the thread so the context is intact (wait)
+                    Console.WriteLine("This one will keep listening. Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+
+        }
+
+        private static void Consumer_Received(object sender, BasicDeliverEventArgs e)
+        {
+            var body = e.Body.ToArray();
+            var message = Encoding.UTF8.GetString(body); // get as string; you could deserialize with json eventually
+            Console.WriteLine($"Received message: {message}.");
+        }
+    }
+}
---
+++ RabbitMqIntro/RabbitMqIntro.Send/Program.cs
@@ -0,0 +1,39 @@
+﻿using RabbitMQ.Client;
+using System;
+using System.Text;
+
+namespace RabbitMqIntro.Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    // declare the queue (idempotent -> same action always; if it exists, it uses it)
+                    channel.QueueDeclare(queue: "hello",
+                                        durable: false,
+                                        exclusive: false,
+                                        autoDelete: false,
+                                        arguments: null);
+
+                    // we can send only bytes
+                    var message = "Hello haters!";
+                    var body = Encoding.UTF8.GetBytes(message);
+                    channel.BasicPublish(exchange: "",
+                                        routingKey: "hello",
+                                        basicProperties: null,
+                                        body: body);
+
+                    Console.WriteLine($"[x] Sent {message}.");
+                }
+            }
+            // here, the publisher does not keep the connection anymore, everything is disposed (but the broker is there)
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();
+        }
+    }
+}
